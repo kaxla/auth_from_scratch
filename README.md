86e95f927ca73f48cd361056a1fae237e606d9cf
kaxla <kaylas.email.address@gmail.com>
2014-02-01 15:55:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Auth from scratch
+
+By [Kayla Morrison]
+
+## Description
+simple blog platform with sign up/in/out functionality built from scratch.
+
+
+## Authors
+
+* Kayla Morrison (github.com/kaxla) following Railscast #250
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = 'cf0adc0ebdd76381844e17513204e65eb7a1c90f1f620fa338c158be7837d87115d23fdc8d25832f9c12c18f8aacc4249058cdc4e9d22282ad7634c71650c47b'
